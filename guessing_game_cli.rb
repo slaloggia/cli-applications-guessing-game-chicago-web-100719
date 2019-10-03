352d9a3ecfe5dc7dd55264d4b06c7cbbd29c1a87
Sarah Laloggia <sarah.laloggia@gmail.com>
2019-10-03 00:15:17 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,13 +1,15 @@
 def run_guessing_game
   answer = rand(1..6)
-  
+  prompt_player
   guess = gets.chomp!
 if answer == guess
   p "You guessed the correct number!"
 end
 end
  
-
+def prompt_player
+  puts "Guess a number between 1 & 6"
+end
   
   
 
