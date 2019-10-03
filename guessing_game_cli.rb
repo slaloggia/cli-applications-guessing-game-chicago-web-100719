8cceae40bbbd0e2831677e116dc1d094fabd7b9b
Sarah Laloggia <sarah.laloggia@gmail.com>
2019-10-03 00:33:46 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,7 +7,7 @@ def run_guessing_game
   elsif guess == "exit"
     puts "Goodbye!"
   else
-    puts "Sorry! The computer guessed #{answer}"
+    puts "Sorry! The computer guessed #{answer}."
   end
 end
  
